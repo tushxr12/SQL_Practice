c822f44dabdbbc68cdc68bdda1d44be8f7f1fa0d
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2026-02-22 14:47:32 +0530

    practice SQL

---
+++ 7. SQL Joins/Problems_Level_3/Updated Bank Balances.sql
@@ -0,0 +1,11 @@
+SELECT 
+    u.name, 
+    COALESCE(SUM(t.amount), 0) AS balance
+FROM 
+    Users u
+LEFT JOIN 
+    Transactions t ON u.account = t.account
+GROUP BY 
+    u.account, u.name
+HAVING 
+    SUM(t.amount) > 10000;
